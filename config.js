d34bf1888c448d98f37ab650f4060cc67214dcd8
apaghcm-hienlv <apag.hcm@gmail.com>
2026-08-27 12:27:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.SUPABASE_CONFIG = {
   URL: "https://adypcfrunxdphknyytqs.supabase.co",
-  ANON_KEY: "DÁN_PUBLISHABLE_KEY_SUPABASE_CỦA_BẠN_VÀO_ĐÂY"
+  ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkeXBjZnJ1bnhkcGhrbnl5dHFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3OTU4NTIsImV4cCI6MjEwMzM3MTg1Mn0.atn3GPuWnGtsFqKVKYZTdhVGXAsW86Eghvg0MEt4pl4"
 };
 
 window.db = supabase.createClient(window.SUPABASE_CONFIG.URL, window.SUPABASE_CONFIG.ANON_KEY);
